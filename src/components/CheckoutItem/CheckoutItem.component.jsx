edb636e0f665e87522f171af287541fd683bbdd9
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-07-14 10:21:09 +0300

    refactor checkout to styled component

--- src/components/CheckoutItem/CheckoutItem.component.jsx
+++ src/components/CheckoutItem/CheckoutItem.component.jsx
@@ -1,12 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styled, { css } from 'styled-components';
 
 import {
 	clearItemFromCart,
 	addItemToCart,
 	removeItemFromCart,
 } from '../../redux/cart/cartActions';
-import './CheckoutItem.styles.scss';
+
+const CheckoutItemContainer = styled.div`
+	width: 100%;
+	display: flex;
+	min-height: 100px;
+	border-bottom: 1px solid darkgrey;
+	padding: 15px 0;
+	font-size: 20px;
+	align-items: center;
+`;
+
+const ImageContainer = styled.div`
+	width: 23%;
+	padding-right: 15px;
+
+	img {
+		display: block;
+		width: 100%;
+		height: 100%;
+	}
+`;
+
+const quantityStyles = css`
+	display: flex;
+
+	div {
+		cursor: pointer;
+	}
+
+	span {
+		margin: 0 10px;
+	}
+`;
+
+const CheckoutProp = styled.span`
+	width: 23%;
+	${(isQuantity) => isQuantity && quantityStyles}
+`;
+
+const RemoveButton = styled.div`
+	padding-left: 12px;
+	cursor: pointer;
+`;
 
 const CheckoutItem = ({
 	item,
@@ -17,25 +60,21 @@ const CheckoutItem = ({
 	const { imageUrl, name, price, quantity } = item;
 
 	return (
-		<div className='checkout-item'>
-			<div className='image-container'>
+		<CheckoutItemContainer>
+			<ImageContainer>
 				<img src={imageUrl} alt='Cart Item' />
-			</div>
-			<span className='name'>{name}</span>
-			<span className='quantity'>
-				<div onClick={() => removeItemFromCart(item)} className='arrow'>
-					&#10094;
-				</div>
-				<span className='value'>{quantity}</span>
-				<div onClick={() => addItemToCart(item)} className='arrow'>
-					&#10095;
-				</div>
-			</span>
-			<span className='price'>{price}</span>
-			<div onClick={() => clearItemFromCart(item)} className='remove-button'>
+			</ImageContainer>
+			<CheckoutProp>{name}</CheckoutProp>
+			<CheckoutProp isQuantity>
+				<div onClick={() => removeItemFromCart(item)}>&#10094;</div>
+				<span>{quantity}</span>
+				<div onClick={() => addItemToCart(item)}>&#10095;</div>
+			</CheckoutProp>
+			<CheckoutProp>{price}</CheckoutProp>
+			<RemoveButton onClick={() => clearItemFromCart(item)}>
 				&#10005;
-			</div>
-		</div>
+			</RemoveButton>
+		</CheckoutItemContainer>
 	);
 };
 
--- src/pages/Checkout/Checkout.component.jsx
+++ src/pages/Checkout/Checkout.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import styled from 'styled-components';
 
 import {
 	selectCartItems,
@@ -8,40 +9,83 @@ import {
 } from '../../redux/cart/cartSelectors';
 import CheckoutItem from '../../components/CheckoutItem/CheckoutItem.component';
 import StripeButton from '../../components/StripeButton/StripeButton.component';
-import './Checkout.styles.scss';
+
+const CheckoutContainer = styled.div`
+	width: 55%;
+	min-height: 90vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin: 50px auto 0;
+
+	button {
+		margin-left: auto;
+		margin-top: 50px;
+	}
+`;
+
+const Header = styled.header`
+	width: 100%;
+	padding: 10px 0;
+	display: flex;
+	justify-content: space-between;
+	border-bottom: 1px solid darkgrey;
+`;
+
+const HeaderBlock = styled.div`
+	text-transform: capitalize;
+	width: 23%;
+
+	&:last-child {
+		width: 8%;
+	}
+`;
+
+const Total = styled.div`
+	margin-top: 30px;
+	margin-left: auto;
+	font-size: 36px;
+`;
+
+const TestWarning = styled.div`
+	text-align: center;
+	margin-top: 40px;
+	font-size: 24px;
+	color: red;
+`;
 
 const Checkout = ({ items, total }) => (
-	<div className='checkout'>
-		<header className='header'>
-			<div className='header-block'>
+	<CheckoutContainer>
+		<Header>
+			<HeaderBlock>
 				<span>Product</span>
-			</div>
-			<div className='header-block'>
+			</HeaderBlock>
+			<HeaderBlock>
 				<span>Description</span>
-			</div>
-			<div className='header-block'>
+			</HeaderBlock>
+			<HeaderBlock>
 				<span>Quantity</span>
-			</div>
-			<div className='header-block'>
+			</HeaderBlock>
+			<HeaderBlock>
 				<span>Price</span>
-			</div>
-			<div className='header-block'>
+			</HeaderBlock>
+			<HeaderBlock>
 				<span>Remove</span>
-			</div>
-		</header>
+			</HeaderBlock>
+		</Header>
 		{items.map((item) => (
 			<CheckoutItem key={item.id} item={item} />
 		))}
-		<div className='total'>
+		<Total>
 			<span>TOTAL: ${total}</span>
-		</div>
-		<div className='test-warning'>
+		</Total>
+		<TestWarning>
 			*Please use the following credit card for test payments*
 			<br />
 			4242 4242 4242 4242 - Exp: 01/24 - CVV: 123
-		</div>
+		</TestWarning>
 		<StripeButton price={total} />
-	</div>
+	</CheckoutContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
