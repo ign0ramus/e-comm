1e93caa9a60574d22709301cabece6e0b724e445
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-07-16 13:07:52 +0300

    add useEffect hook to shop

--- src/pages/Shop/Shop.components.jsx
+++ src/pages/Shop/Shop.components.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,30 +7,26 @@ import CollectionContainer from '../Collection/Collection.container';
 
 import { fetchCollectionsStart } from '../../redux/shop/shopActions';
 
-class Shop extends Component {
-	componentDidMount() {
-		this.props.fetchCollections();
-	}
+const Shop = ({ fetchCollections, match }) => {
+	useEffect(() => {
+		fetchCollections();
+	}, [fetchCollections]);
 
-	render() {
-		const { match } = this.props;
-
-		return (
-			<div className='shop'>
-				<Route
-					exact
-					path={`${match.path}`}
-					component={CollectionOverviewContainer}
-				/>
-				<Route
-					exact
-					path={`${match.path}/:collectionId`}
-					component={CollectionContainer}
-				/>
-			</div>
-		);
-	}
-}
+	return (
+		<div className='shop'>
+			<Route
+				exact
+				path={`${match.path}`}
+				component={CollectionOverviewContainer}
+			/>
+			<Route
+				exact
+				path={`${match.path}/:collectionId`}
+				component={CollectionContainer}
+			/>
+		</div>
+	);
+};
 
 const mapDispatchToProps = (dispatch) => ({
 	fetchCollections: () => dispatch(fetchCollectionsStart()),
