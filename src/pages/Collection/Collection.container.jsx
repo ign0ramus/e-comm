fde24b69e6c23e9f93c6a79e5d1d10205d8ffc1c
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-07-16 16:25:12 +0300

    minor fix

--- src/pages/Collection/Collection.container.jsx
+++ src/pages/Collection/Collection.container.jsx
@@ -4,10 +4,10 @@ import { createStructuredSelector } from 'reselect';
 
 import { selectIsCollectionsLoaded } from '../../redux/shop/shopSelectors';
 import WithSpinner from '../../hocs/WithSpinner/WithSpinner.component';
-import Colletion from './Collection.component';
+import Collection from './Collection.component';
 
 const mapStateToProps = createStructuredSelector({
 	isLoading: (state) => !selectIsCollectionsLoaded(state),
 });
 
-export default compose(connect(mapStateToProps), WithSpinner)(Colletion);
+export default compose(connect(mapStateToProps), WithSpinner)(Collection);
