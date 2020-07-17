a4b6ff7bc573b827cc45546051badf4829f7e38e
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-07-17 11:03:48 +0300

    add styled component global styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { createGlobalStyle } from 'styled-components';
 
 import Home from './pages/Home/Home.component';
 import Shop from './pages/Shop/Shop.components';
@@ -10,7 +11,22 @@ import Checkout from './pages/Checkout/Checkout.component';
 
 import { selectUser } from './redux/user/userSelectors';
 import { checkUserSession } from './redux/user/userActions';
-import './App.css';
+
+const GlobalStyles = createGlobalStyle`
+	* {
+		box-sizing: border-box;
+	}
+
+	body {
+		font-family: 'Open Sans Condensed', sans-serif;
+		padding: 20px 60px;
+	}
+
+	a {
+		text-decoration: none;
+		color: #000;
+	}
+`;
 
 const App = () => {
 	const user = useSelector(selectUser);
@@ -22,6 +38,7 @@ const App = () => {
 
 	return (
 		<div>
+			<GlobalStyles />
 			<Header />
 			<Switch>
 				<Route exact path='/' component={Home} />
