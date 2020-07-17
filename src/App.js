ea64fe37bf1d3094f4659258bbfef7c952dc4ddd
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-07-17 11:07:44 +0300

    add mobile global styles

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ const GlobalStyles = createGlobalStyle`
 	body {
 		font-family: 'Open Sans Condensed', sans-serif;
 		padding: 20px 60px;
+
+		@media screen and (max-width: 800px) {
+			padding: 10px;
+		}
 	}
 
 	a {
