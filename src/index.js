0ad674115cafd93f6e7c91afc2d8cd06c75824cd
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-07-18 11:39:43 +0300

    add pwa functionallity

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import { store, persistor } from './redux/store';
 
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
@@ -17,3 +19,5 @@ ReactDOM.render(
 	</Provider>,
 	document.getElementById('root')
 );
+
+serviceWorker.register();
